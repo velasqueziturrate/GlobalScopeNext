b71ba8954e8695e98d81d1013ed837de2b23d2d3
“velasqueziturrate” <daniel.velasquez@correo.unimet.edu.ve>
2020-09-01 15:25:34 -0400

    Septimo Commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // styles 
-import './assets/css/bootstrap.min.css'; 
+import '../assets/css/bootstrap.min.css';
 // import "./../node_modules/font-awesome/css/font-awesome.min.css";
 import 'font-awesome/css/font-awesome.min.css';
 import './assets/sass/index.scss';
